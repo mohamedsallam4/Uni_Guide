33d06988c0a4ffdc196d15bb76cdfa8111eb93ac
Ahmed Ewaida <ahmedewaida234@gmail.com>
2026-04-19 01:14:57 +0200

    extract all colors in app

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFF1E88E5);
-  static const Color secondary = Color(0xFF42A5F5);
-  static const Color accent = Color(0xFF82B1FF);
-  static const Color background = Color(0xFFFFFFFF);
-  static const Color surface = Color(0xFFF5F5F5);
-  static const Color error = Color(0xFFD32F2F);
-  static const Color onPrimary = Color(0xFFFFFFFF);
-  static const Color onSecondary = Color(0xFF000000);
-  static const Color onBackground = Color(0xFF000000);
-  static const Color onSurface = Color(0xFF000000);
-  static const Color onError = Color(0xFFFFFFFF);
+  static const Color primary = Color(0xFFAFEC70);
+  static const Color secondary = Color(0xFF154618);
+  static const Color primaryLight = Color(0xFFF6FEEB);
+  static final Color primaryDark = Color(0xFF154618);
+  static const Color onBackground = Color(0xFFFFFFFF);
+  static const Color onSubText = Color(0xFF6A7282);
+  static const Color onSubTextTab = Color(0xFF99A1AF);
+  static const Color onSubTextBackground = Color(0xFFF3F4F6);
+  static const Color cancelText = Color(0xFFE7000B);
+  static const Color cancelBackground = Color(0xFFFFE2E2);
+  static const Color acceptText = Color(0xFF008509);
+  static const Color acceptBackground = Color(0xFFE2FFE2);
+  static const Color saveText = Color(0xFF004F3B);
+  static const Color saveSubText = Color(0xFF007A55);
+  static const Color saveBackground = Color(0xFFA4F4CF);
+  static const Color updateText = Color(0xFF1C398E);
+  static const Color updateSubText = Color(0xFF1447E6);
+  static const Color updateBackground = Color(0xFFBEDBFF);
+  static const Color onError = Color(0xFFFF2056);
+  static const Color onErrorBackground = Color(0xFFFFE4E6);
+  static const Color black = Color(0xFF000000);
 }
